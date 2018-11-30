3f3df7ebfa6ae2c78e58559ed672a264336c851c
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-30 14:24:55 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+i = 0 
+while i < collection.length
+return collection[i] 
+if yield(collection[i])
+  
+end 
+i += 1
+end 
+my_find(collection).detect {|i| i % 3 == 0 && i % 5 == 0}
 end
